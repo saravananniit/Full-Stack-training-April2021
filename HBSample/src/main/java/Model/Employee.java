c58aed8cb0b3269f345fa78637731d6a334a3c86
saravanan.1.p <saravanan.1.p@CHN-CEB-326377.in.niit.com>
2021-06-02 14:10:00 +0530

    Hibernate demo

---
+++ HBSample/src/main/java/Model/Employee.java
@@ -0,0 +1,33 @@
+package Model;
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Employee {
+
+	@Id
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+	
+	//this is an model for database program
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	private String name;
+	
+	
+
+	
+}
---
+++ HBSample/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+welcome to index page
+
+<form action="./call" method="post">
+
+Enter the Id here <input type="text" name="t1"/>
+<br/>
+Enter the password here <input type="text" name="t2"/>
+<br/>
+<input type="submit" value="click here"/>
+
+</form>
+
+
+</body>
+</html>
